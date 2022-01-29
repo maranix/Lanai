c3f8b0290a586f652a3bfcd338567cd5712cf93e
ramanverma2k <ramanverma4183@gmail.com>
2022-01-29 18:53:04 +0530

    domain: cache_provider: invalidate cache and refetch new data every 4hrs

--- lib/domain/services/cache_provider.dart
+++ lib/domain/services/cache_provider.dart
@@ -6,11 +6,20 @@ abstract class Cache {}
 class ApiCacheProvider {
   static void cacheRequest(String key, String value) async {
     final _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.setString('cacheTime', DateTime.now().toString());
     _sharedPreferences.setString(key, value);
   }
 
   static Future<String?> getCachedRequest(String key) async {
     final _sharedPreferences = await SharedPreferences.getInstance();
-    return _sharedPreferences.getString(key);
+
+    final _cacheTime = _sharedPreferences.getString('cacheTime');
+    final _elapsedTime =
+        DateTime.now().difference(DateTime.parse(_cacheTime!)).inHours;
+    if (_elapsedTime >= 4) {
+      await _sharedPreferences.clear();
+    } else {
+      return _sharedPreferences.getString(key);
+    }
   }
 }
