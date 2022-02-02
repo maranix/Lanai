b8ad503f6d4a3d8602170406816136dd4b5aced1
ramanverma2k <ramanverma4183@gmail.com>
2022-02-02 11:07:02 +0530

    home_page: hide appbar on scroll

--- lib/application/screens/home_page.dart
+++ lib/application/screens/home_page.dart
@@ -25,110 +25,117 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Lanai',
-          style: Style.textStyleHeading,
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const SearchPage(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.search_rounded),
-            color: Colors.black,
+      backgroundColor: const Color(0xFFFFFEF4),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            title: const Text(
+              'Lanai',
+              style: Style.textStyleHeading,
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const SearchPage(),
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.search_rounded),
+                color: Colors.black,
+              ),
+            ],
+            toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+            elevation: 0,
+            backgroundColor: const Color(0xFFFFFEF4),
           ),
         ],
-        toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-        elevation: 0,
-        backgroundColor: const Color(0xFFFFFEF4),
-      ),
-      backgroundColor: const Color(0xFFFFFEF4),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ListView.separated(
-                    itemBuilder: (context, index) => TextButton(
-                      onPressed: () {
-                        setState(() {
-                          selectedCategory = Constants.categoryList[index];
-                        });
-                      },
-                      child: Text(
-                        Constants.categoryList[index],
-                        style: TextStyle(
-                          color:
-                              selectedCategory == Constants.categoryList[index]
-                                  ? Colors.black
-                                  : Colors.grey,
-                          fontWeight: FontWeight.w600,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ListView.separated(
+                      itemBuilder: (context, index) => TextButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedCategory = Constants.categoryList[index];
+                          });
+                        },
+                        child: Text(
+                          Constants.categoryList[index],
+                          style: TextStyle(
+                            color: selectedCategory ==
+                                    Constants.categoryList[index]
+                                ? Colors.black
+                                : Colors.grey,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(width: 10),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: Constants.categoryList.length,
                     ),
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(width: 10),
-                    scrollDirection: Axis.horizontal,
-                    itemCount: Constants.categoryList.length,
                   ),
-                ),
-                Constants.gap,
-                selectedCategory == 'Nature'
-                    ? CategoryShowCase(
-                        photos:
-                            Provider.of<PhotoProvider>(context).naturePhotos,
-                      )
-                    : selectedCategory == 'Light'
-                        ? CategoryShowCase(
-                            photos: Provider.of<PhotoProvider>(context)
-                                .lightsPhotos,
-                          )
-                        : selectedCategory == 'Texture'
-                            ? CategoryShowCase(
-                                photos: Provider.of<PhotoProvider>(context)
-                                    .texturePhotos,
-                              )
-                            : selectedCategory == 'Raining'
-                                ? CategoryShowCase(
-                                    photos: Provider.of<PhotoProvider>(context)
-                                        .rainingPhotos,
-                                  )
-                                : selectedCategory == 'Adventure'
-                                    ? CategoryShowCase(
-                                        photos:
-                                            Provider.of<PhotoProvider>(context)
-                                                .adventurePhotos,
-                                      )
-                                    : selectedCategory == 'Dark'
-                                        ? CategoryShowCase(
-                                            photos: Provider.of<PhotoProvider>(
-                                                    context)
-                                                .darkPhotos,
-                                          )
-                                        : selectedCategory == 'Retro'
-                                            ? CategoryShowCase(
-                                                photos:
-                                                    Provider.of<PhotoProvider>(
-                                                            context)
-                                                        .retroPhotos,
-                                              )
-                                            : const Center(
-                                                child: CircularProgressIndicator
-                                                    .adaptive(),
-                                              ),
-              ],
+                  Constants.gap,
+                  selectedCategory == 'Nature'
+                      ? CategoryShowCase(
+                          photos:
+                              Provider.of<PhotoProvider>(context).naturePhotos,
+                        )
+                      : selectedCategory == 'Light'
+                          ? CategoryShowCase(
+                              photos: Provider.of<PhotoProvider>(context)
+                                  .lightsPhotos,
+                            )
+                          : selectedCategory == 'Texture'
+                              ? CategoryShowCase(
+                                  photos: Provider.of<PhotoProvider>(context)
+                                      .texturePhotos,
+                                )
+                              : selectedCategory == 'Raining'
+                                  ? CategoryShowCase(
+                                      photos:
+                                          Provider.of<PhotoProvider>(context)
+                                              .rainingPhotos,
+                                    )
+                                  : selectedCategory == 'Adventure'
+                                      ? CategoryShowCase(
+                                          photos: Provider.of<PhotoProvider>(
+                                                  context)
+                                              .adventurePhotos,
+                                        )
+                                      : selectedCategory == 'Dark'
+                                          ? CategoryShowCase(
+                                              photos:
+                                                  Provider.of<PhotoProvider>(
+                                                          context)
+                                                      .darkPhotos,
+                                            )
+                                          : selectedCategory == 'Retro'
+                                              ? CategoryShowCase(
+                                                  photos: Provider.of<
+                                                              PhotoProvider>(
+                                                          context)
+                                                      .retroPhotos,
+                                                )
+                                              : const Center(
+                                                  child:
+                                                      CircularProgressIndicator
+                                                          .adaptive(),
+                                                ),
+                ],
+              ),
             ),
           ),
         ),
