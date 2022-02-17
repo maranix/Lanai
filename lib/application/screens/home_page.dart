ebe944ab182ffad33cb27155d114c9d1f9bf4a47
ramanverma2k <ramanverma4183@gmail.com>
2022-02-17 13:54:14 +0530

    photo_provider: switch to StateNotifier and use LocatorMixin for dependency injection.

--- lib/application/screens/home_page.dart
+++ lib/application/screens/home_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:http/http.dart' as http;
+import 'package:lanai/domain/models/photo_model.dart';
+import 'package:lanai/domain/services/photos_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: StateNotifierBuilder<Photo>(
+          stateNotifier: PhotosProvider(http.Client())..getPhoto(),
+          builder: (BuildContext context, Photo photo, Widget? child) {
+            return photo.map(
+              data: (state) => Image.network(
+                state.src.large2x,
+                fit: BoxFit.contain,
+              ),
+              loading: (state) => const Text('Loading'),
+              error: (state) => Text('Error: ${state.error.toString()}'),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/domain/services/photos_provider.dart
+++ lib/domain/services/photos_provider.dart
@@ -1,7 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-// TODO: Implement PhotoProvider to retrieve photos from the api.
+import 'package:http/http.dart' as http;
+import 'package:lanai/data/constants.dart';
+import 'package:lanai/domain/models/photo_model.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+// TODO: Implement PhotoProvider to retrieve photos from the api and rename it to better reflect the usage.
 /// # PhotosProvider
-/// The following class uses [ChangeNotifier] to ensure that
+/// The following class uses [StateNotifier] to ensure that
 /// the dependent widgets are notified whenever the photos are loaded or updated.
-class PhotosProvider extends ChangeNotifier {}
+class PhotosProvider extends StateNotifier<Photo> with LocatorMixin {
+  PhotosProvider(this.httpClient) : super(const Photo.loading());
+
+  final http.Client httpClient;
+
+  Future<void> getPhoto() async {
+    final res = await httpClient.get(
+      Uri.parse('$apiPREFIX/photos/2014422'),
+      headers: {
+        'Authorization': apiKEY,
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (res.statusCode == 200) {
+      state = Photo.fromJson(jsonDecode(res.body));
+    } else {
+      state = Photo.error('${res.statusCode}');
+      throw Exception("Error getting photo");
+    }
+  }
+}
