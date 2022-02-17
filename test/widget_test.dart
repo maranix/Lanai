220c5a1d4b66631add069ccbb28a3f2623eca913
ramanverma2k <ramanverma4183@gmail.com>
2022-02-17 19:36:32 +0530

    test: PhotoNotifier loading state

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,25 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:lanai/application/app.dart';
+import 'package:lanai/data/repositories/photos_repository.dart';
+import 'package:http/http.dart' as http;
+import 'package:lanai/domain/models/photo_model.dart';
+import 'package:lanai/domain/services/photos_provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+  group(
+    "PhotoNotifier",
+    () {
+      late PhotoNotifier photosNotifier;
+      setUp(() async {
+        photosNotifier = PhotoNotifier(PhotosRepository(http.Client()))
+          ..debugMockDependency<Photo>(const Photo.loading());
+      });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      test("returns Photo loading state", () async {
+        expect(photosNotifier.debugState, const Photo.loading());
+      });
+    },
+  );
 }
