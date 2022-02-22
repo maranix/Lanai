577f8c87aff3fec7e2d9d63709d4388a20dd6e6f
ramanverma2k <ramanverma4183@gmail.com>
2022-02-22 12:06:43 +0530

    style: add gradient colors for Collection Showcase

--- lib/application/theme/style.dart
+++ lib/application/theme/style.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+List<LinearGradient> gradients = const [
+  LinearGradient(colors: [Color(0xFFff1b6b), Color(0xFF45caff)]),
+  LinearGradient(colors: [Color(0xFF40c9ff), Color(0xFFe81cff)]),
+  LinearGradient(colors: [Color(0xFF696eff), Color(0xFFf8acff)]),
+  LinearGradient(
+      colors: [Color(0xFF84ffc9), Color(0xFFaab2ff), Color(0xFFeca0ff)]),
+  LinearGradient(
+    colors: [Color(0xFFb2ef91), Color(0xFFfa9372)],
+  )
+];
+
 class Style {
   static const textStyleHeading = TextStyle(
     color: Color(0xFF000000),
