4894d6d4a6baebd7e9400965c96965f5fd3db13a
ramanverma2k <ramanverma4183@gmail.com>
2022-02-22 13:19:47 +0530

    Display all images/videos upon clicking see all on homepage.

--- lib/application/screens/home_page.dart
+++ lib/application/screens/home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:http/http.dart' as http;
+import 'package:lanai/application/constants/constants.dart';
+import 'package:lanai/application/screens/see_all.dart';
 import 'package:lanai/application/widgets/app_bar.dart';
-import 'package:lanai/data/repositories/collections_repository.dart';
 import 'package:lanai/data/repositories/photos_repository.dart';
 import 'package:lanai/data/repositories/videos_repository.dart';
-import 'package:lanai/domain/models/collections_model.dart';
 import 'package:lanai/domain/models/photo_model.dart';
 import 'package:lanai/domain/models/video_model.dart';
-import 'package:lanai/domain/services/collections_notifier.dart';
 import 'package:lanai/domain/services/photos_notifier.dart';
 import 'package:lanai/domain/services/video_notifier.dart';
 
@@ -18,13 +17,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final photoNotifier = PhotoNotifier(PhotosRepository(http.Client()));
-    final collectionNotifier =
-        CollectionsNotifier(CollectionsRepository(http.Client()));
     final videoNotifier = VideosNotifier(VideosRepository(http.Client()));
     photoNotifier.curated();
-    collectionNotifier.featuredCollection();
     videoNotifier.popular();
-
     final _scrollController = ScrollController();
 
     return Scaffold(
@@ -42,76 +37,18 @@ class HomePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const Text(
-                        'Featured Collections',
-                        style: TextStyle(fontSize: 45),
-                      ),
-                      GestureDetector(
-                        child: const Text(
-                          'See All',
-                          style:
-                              TextStyle(fontSize: 20, color: Colors.lightBlue),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                StateNotifierBuilder<List<Collection>>(
-                  stateNotifier: collectionNotifier,
-                  builder: (context, state, child) => state.isNotEmpty
-                      ? SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 20,
-                              mainAxisSpacing: 20,
-                            ),
-                            itemBuilder: (context, index) {
-                              return state[index].map(
-                                data: (collection) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 12.0),
-                                  child: DecoratedBox(
-                                    decoration: const BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(15))),
-                                    child: Center(
-                                      child: Text(collection.title),
-                                    ),
-                                  ),
-                                ),
-                                error: (error) => Text(error.error.toString()),
-                                loading: (CollectionLoading value) {
-                                  return const Text('loading');
-                                },
-                              );
-                            },
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 4,
-                          ),
-                        )
-                      : const Center(
-                          child: CircularProgressIndicator.adaptive(),
-                        ),
-                ),
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
                         'Curated Photos',
                         style: TextStyle(fontSize: 45),
                       ),
                       GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SeeAll(
+                                      data: photoNotifier,
+                                      section: 'Curated Photos')));
+                        },
                         child: const Text(
                           'See All',
                           style:
@@ -124,37 +61,39 @@ class HomePage extends StatelessWidget {
                 StateNotifierBuilder<List<Photo>>(
                   stateNotifier: photoNotifier,
                   builder: (context, state, child) => state.isNotEmpty
-                      ? SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 20,
-                              mainAxisSpacing: 20,
-                            ),
-                            itemBuilder: (context, index) {
-                              return state[index].map(
-                                data: (photo) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 12.0),
-                                  child: DecoratedBox(
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 12.0),
+                          child: SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.45,
+                            child: GridView.builder(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 15,
+                                crossAxisSpacing: 10,
+                              ),
+                              itemBuilder: (context, index) {
+                                return state[index].map(
+                                  data: (photo) => DecoratedBox(
                                     decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(15)),
                                       image: DecorationImage(
                                         image: NetworkImage(photo.src.portrait),
                                         fit: BoxFit.cover,
                                       ),
                                     ),
                                   ),
-                                ),
-                                error: (error) => Text(error.error.toString()),
-                                loading: (PhotoLoading value) {
-                                  return const Text('loading');
-                                },
-                              );
-                            },
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 4,
+                                  error: (error) =>
+                                      Text(error.error.toString()),
+                                  loading: (PhotoLoading value) {
+                                    return const Text('loading');
+                                  },
+                                );
+                              },
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 5,
+                            ),
                           ),
                         )
                       : const Center(
@@ -178,6 +117,14 @@ class HomePage extends StatelessWidget {
                         style: TextStyle(fontSize: 45),
                       ),
                       GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SeeAll(
+                                      data: videoNotifier,
+                                      section: 'Popular Videos')));
+                        },
                         child: const Text(
                           'See All',
                           style:
@@ -190,37 +137,39 @@ class HomePage extends StatelessWidget {
                 StateNotifierBuilder<List<Video>>(
                   stateNotifier: videoNotifier,
                   builder: (context, state, child) => state.isNotEmpty
-                      ? SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 20,
-                              mainAxisSpacing: 20,
-                            ),
-                            itemBuilder: (context, index) {
-                              return state[index].map(
-                                data: (video) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 12.0),
-                                  child: DecoratedBox(
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 12.0),
+                          child: SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.45,
+                            child: GridView.builder(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 15,
+                                crossAxisSpacing: 10,
+                              ),
+                              itemBuilder: (context, index) {
+                                return state[index].map(
+                                  data: (video) => DecoratedBox(
                                     decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(15)),
                                       image: DecorationImage(
                                         image: NetworkImage(video.image),
                                         fit: BoxFit.cover,
                                       ),
                                     ),
                                   ),
-                                ),
-                                error: (error) => Text(error.error.toString()),
-                                loading: (VideoLoading value) {
-                                  return const Text('loading');
-                                },
-                              );
-                            },
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 4,
+                                  error: (error) =>
+                                      Text(error.error.toString()),
+                                  loading: (VideoLoading value) {
+                                    return const Text('loading');
+                                  },
+                                );
+                              },
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 5,
+                            ),
                           ),
                         )
                       : const Center(
@@ -229,7 +178,8 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          const SliverToBoxAdapter(child: Constants.gap20h)
         ],
       ),
     );
---
+++ lib/application/screens/see_all.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:lanai/domain/models/photo_model.dart';
+
+class SeeAll extends StatelessWidget {
+  const SeeAll({Key? key, required this.data, this.section}) : super(key: key);
+
+  final dynamic data;
+  final String? section;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                section!,
+                style: const TextStyle(
+                  fontSize: 50,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                size: 30,
+                color: Colors.black,
+              ),
+            ),
+            backgroundColor: Colors.grey.shade50,
+            elevation: 0,
+          ),
+          StateNotifierBuilder<List>(
+            stateNotifier: data,
+            builder: (BuildContext context, state, Widget? child) {
+              return state.isNotEmpty
+                  ? SliverPadding(
+                      padding: const EdgeInsets.all(12.0),
+                      sliver: SliverGrid(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) => state[index].mapOrNull(
+                              data: (result) => DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(15)),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          result.runtimeType == Photo
+                                              ? result.src.portrait
+                                              : result.image,
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                              error: (error) => Text(error.error.toString())),
+                          childCount: state.length,
+                        ),
+                        gridDelegate: SliverQuiltedGridDelegate(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          repeatPattern: QuiltedGridRepeatPattern.inverted,
+                          pattern: const [
+                            QuiltedGridTile(1, 2),
+                            QuiltedGridTile(1, 1),
+                            QuiltedGridTile(2, 1),
+                            QuiltedGridTile(1, 1),
+                          ],
+                        ),
+                      ),
+                    )
+                  : const SliverFillViewport(
+                      delegate: SliverChildListDelegate.fixed(
+                        [
+                          Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          )
+                        ],
+                      ),
+                    );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
