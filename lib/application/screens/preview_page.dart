5dea22d5e2f8215a1fcc535d36cb82d1279f633d
ramanverma2k <ramanverma4183@gmail.com>
2022-02-23 11:39:42 +0530

    add a photo preview page and fab button to take different actions.

---
+++ lib/application/screens/preview_page.dart
@@ -0,0 +1,29 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:lanai/application/widgets/preview_page_fab.dart';
+
+class PreviewPage extends StatelessWidget {
+  const PreviewPage({Key? key, required this.data}) : super(key: key);
+
+  final dynamic data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          ExtendedImage.network(
+            data.src.original,
+            cache: true,
+            fit: BoxFit.cover,
+            clearMemoryCacheIfFailed: true,
+            enableMemoryCache: true,
+          ),
+        ],
+      ),
+      floatingActionButton: PreviewFAB(data: data),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
+  }
+}
--- lib/application/widgets/curated_photos_widget.dart
+++ lib/application/widgets/curated_photos_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lanai/application/screens/preview_page.dart';
 
 class CuratedPhotosWidget extends StatelessWidget {
   const CuratedPhotosWidget({Key? key, this.photo}) : super(key: key);
@@ -7,12 +8,22 @@ class CuratedPhotosWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(15)),
-        image: DecorationImage(
-          image: NetworkImage(photo.src.portrait),
-          fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PreviewPage(data: photo),
+          ),
+        );
+      },
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+          image: DecorationImage(
+            image: NetworkImage(photo.src.portrait),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
