e015608e4a8a3916deeef4f88479c82d11a38317
ramanverma2k <ramanverma4183@gmail.com>
2022-08-18 22:43:23 +0530

    ui: add search page and bloc

---
+++ lib/search/view/search_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pexels_repository/search/search.dart';
+import './search_view.dart';
+
+/// {@template search_page}
+/// A description for SearchPage
+/// {@endtemplate}
+class SearchPage extends StatelessWidget {
+  /// {@macro search_page}
+  const SearchPage({super.key});
+
+  /// The static route for SearchPage
+  /// Can be directly accessed using
+  /// code dart`
+  /// Navigator.push(SearchPage.route());
+  /// `
+  static Route<SearchPage> route() {
+    return MaterialPageRoute<SearchPage>(builder: (_) => const SearchPage(),);
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return BlocProvider<SearchBloc>(
+      create: (BuildContext context) => SearchBloc(),
+      child: const SearchView(),
+    );
+  }
+}
