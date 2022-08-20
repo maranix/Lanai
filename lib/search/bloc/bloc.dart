bf80ce7a520412d6a8b56f57d464ef826aad7fea
ramanverma2k <ramanverma4183@gmail.com>
2022-08-20 12:44:00 +0530

    search: remove module as we do not depend on it for search functionality

--- lib/search/bloc/bloc.dart
+++
@@ -1,2 +0,0 @@
-export 'package:flutter_bloc/flutter_bloc.dart';
-export 'search_bloc.dart';
--- lib/search/search.dart
+++
@@ -1,3 +0,0 @@
-export 'bloc/bloc.dart';
-export 'view/search_page.dart';
-export 'widgets/widgets.dart';
