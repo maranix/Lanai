d3f194c633af13761d07bb70961a1c255cc3887e
ramanverma2k <ramanverma4183@gmail.com>
2022-08-20 10:46:15 +0530

    Settings: move ThemeToggle button to its own seperate file

---
+++ lib/settings/widgets/toggle_theme_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lanai/theme/theme.dart';
+
+class ToggleThemeButton extends StatefulWidget {
+  const ToggleThemeButton({super.key});
+
+  @override
+  State<ToggleThemeButton> createState() => _ToggleThemeButtonState();
+}
+
+class _ToggleThemeButtonState extends State<ToggleThemeButton> {
+  bool isToggled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      value: isToggled,
+      title: const Text('Dark Mode'),
+      onChanged: (value) {
+        setState(() {
+          isToggled = value;
+          context.read<ThemeBloc>().add(
+                const ThemeToggled(),
+              );
+        });
+      },
+    );
+  }
+}
