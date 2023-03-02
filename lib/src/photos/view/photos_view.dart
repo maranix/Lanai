3c48ae30574fb338babf854056ee158a07ec32ee
deveraman <ramanverma4183@gmail.com>
2023-03-03 00:27:34 +0530

    feat(app): initial PhotosScreen UI Skeleton

--- lib/src/photos/view/photos_view.dart
+++ lib/src/photos/view/photos_view.dart
@@ -1,6 +1,8 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lanai/l10n/l10n.dart';
+import 'package:lanai/src/theme/theme.dart';
 
 class PhotosView extends StatelessWidget {
   const PhotosView({super.key});
@@ -24,7 +26,63 @@ class PhotosView extends StatelessWidget {
                 ),
               ),
             ),
+            const NavigationContainer(
+              text: 'Categories',
+              color: Colors.pinkAccent,
+            ),
+            const NavigationContainer(
+              text: 'Collections',
+              color: Colors.indigoAccent,
+            ),
+            const NavigationContainer(
+              text: 'Featured',
+              color: Colors.redAccent,
+            ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<ThemeBloc>().add(
+              const ThemeToggled(),
+            ),
+        child: const Icon(Icons.brightness_7_outlined),
+      ),
+    );
+  }
+}
+
+class NavigationContainer extends StatelessWidget {
+  const NavigationContainer({
+    required this.color,
+    required this.text,
+    this.onTap,
+    super.key,
+  });
+
+  final Color color;
+  final String text;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color,
+        ),
+        child: FittedBox(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: VerticalPadding.y4.copyWith(left: 4, right: 20),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontFamily: 'Playfair Display',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
--- packages/app_ui/lib/src/paddings/vertical_paddings.dart
+++ packages/app_ui/lib/src/paddings/vertical_paddings.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class VerticalPadding {
-  static EdgeInsets get y12 => const EdgeInsets.symmetric(vertical: 12);
+  static EdgeInsets get y4 => const EdgeInsets.symmetric(vertical: 4);
 }
